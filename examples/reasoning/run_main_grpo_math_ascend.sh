e99210410394aaa54e9c66bda9d1eb16b8ffdc48
FxxxxU <fu18801374388@163.com>
2026-07-22 19:17:37 +0800

    test on ascned

--- examples/reasoning/run_main_grpo_math_ascend.sh
+++ examples/reasoning/run_main_grpo_math_ascend.sh
@@ -9,7 +9,7 @@ export HCCL_INTRA_ROCE_ENABLE=1
 
 CONFIG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_PATH=$(dirname $(dirname "$CONFIG_PATH"))
-MEGATRON_PATH=/opt/Megatron-LM
+MEGATRON_PATH=/home/infiniai/zhouyongkang/projects/temp/Megatron-LM
 export PYTHONPATH=${REPO_PATH}:${MEGATRON_PATH}:$PYTHONPATH
 
 #支持NPU单卡多进程
@@ -18,7 +18,7 @@ export HCCL_NPU_SOCKET_PORT_RANGE=61000-61050
 #规避ray在device侧调用无法根据is_npu_available接口识别设备可用性
 export RAY_EXPERIMENTAL_NOSET_ASCEND_RT_VISIBLE_DEVICES=1
 #根据当前设备和需要卡数定义
-export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 #,8,9,10,11,12,13,14,15
 
 if [ -z "$1" ]; then
     CONFIG_NAME="qwen2.5-1.5b-grpo-megatron"
