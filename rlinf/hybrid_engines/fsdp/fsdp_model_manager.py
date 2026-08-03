6351ab61bab4202831eea6570941edfca01530e3
YongkangZhou <zeenny.willians@sjtu.edu.cn>
2026-08-03 17:15:51 +0800

    refactor(ascend): clean up agent integration
    Signed-off-by: YongkangZhou <zeenny.willians@sjtu.edu.cn>

--- rlinf/hybrid_engines/fsdp/fsdp_model_manager.py
+++ rlinf/hybrid_engines/fsdp/fsdp_model_manager.py
@@ -329,7 +329,6 @@ class FSDPModelManager:
                 device_mesh=self._device_mesh,
                 dtype=self.torch_dtype,
             )
-            self._strategy.debug_fsdp2_param_memory(self.model, self.torch_dtype)
 
         self.optimizer = self.build_optimizer(
             model=self.model, enable_critic_warmup=self.critic_warmup_steps > 0
